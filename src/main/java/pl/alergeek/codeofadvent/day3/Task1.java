a910f9bd047ee3da088f77a975621618c181c0e4
alergeek <promucha@gmail.com>
2024-01-06 14:29:19 +0100

    cleanup

--- src/main/java/pl/alergeek/codeofadvent/day3/Task1.java
+++ src/main/java/pl/alergeek/codeofadvent/day3/Task1.java
@@ -6,11 +6,10 @@ import pl.alergeek.model.Task;
 import java.util.List;
 
 public class Task1 implements Task {
-    private EngineSchematic engineSchematic;
 
     @Override
     public String solve(List<String> input) {
-        engineSchematic = EngineSchematic.from(input);
+        EngineSchematic engineSchematic = EngineSchematic.from(input);
         int sum = engineSchematic.sum();
         return String.valueOf(sum);
     }
--- src/main/java/pl/alergeek/codeofadvent/day3/Task2.java
+++ src/main/java/pl/alergeek/codeofadvent/day3/Task2.java
@@ -1,19 +1,15 @@
 package pl.alergeek.codeofadvent.day3;
 
-import pl.alergeek.codeofadvent.day3.model.DigitPart;
 import pl.alergeek.codeofadvent.day3.model.EngineSchematic;
-import pl.alergeek.codeofadvent.day3.model.SymbolPart;
 import pl.alergeek.model.Task;
 
 import java.util.List;
-import java.util.Set;
 
 public class Task2 implements Task {
-    private EngineSchematic engineSchematic;
 
     @Override
     public String solve(List<String> input) {
-        engineSchematic = EngineSchematic.from(input);
+        EngineSchematic engineSchematic = EngineSchematic.from(input);
 
         int sum = engineSchematic.sumGearRation();
         return String.valueOf(sum);
